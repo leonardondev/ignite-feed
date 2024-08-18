dd9d0a59f3020a4be9c7340b561975e735593241
Leonardo do Nascimento <leonardo3.nascimento@gmail.com>
2024-08-17 22:15:57 -0300

    add Ignite Feed header

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./global.css";
+
+import { Header } from "./components/header";
+
+export function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import styles from './Header.module.css';
+
+import igniteImg from '../assets/ignite-logo.svg';
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <img src={igniteImg} alt="Logotipo do Ignite" />
+      <strong>Ignite Feed</strong>
+    </header>
+  )
+}
